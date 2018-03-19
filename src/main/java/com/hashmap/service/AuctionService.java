94928d67e87fbd5cc4d25e14c8671fe9875f3fe2
vikash singh <singhvikash301094@gmail.com>
2018-03-20 00:49:13 +0530

    refactor the methods and added Spring annotations

--- src/main/java/com/hashmap/service/AuctionService.java
+++ src/main/java/com/hashmap/service/AuctionService.java
@@ -9,69 +9,65 @@ import com.hashmap.exception.InvalidAuction;
 import com.hashmap.exception.InvalidBid;
 import com.hashmap.models.auction.Auction;
 import com.hashmap.models.auction.Bid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.UUID;
+import java.util.*;
 
 
+@Service
 public class AuctionService {
 
+    @Autowired
     private AuctionDao auctionDao;
+    @Autowired
     private BidDao bidDao;
+    @Autowired
     private PaymentGateWay paymentGateWay;
-    private  Map<UUID, TimerService> timers = new TreeMap<>();
 
+    private Map<UUID, TimerService> timers = new TreeMap<>();
 
-    //Reduce the dependency,Initialize in constructor,
-    //such that all member function can access it.
-    public AuctionService() {
-        auctionDao =  new AuctionDaoImpl();
-        bidDao = new BidDaoImpl();
-        paymentGateWay = new PaymentGateWayImpl();
+    private AuctionService() {
     }
 
-
-    //Function should be doing one thing
-    //So,we created function StartTimeWithListenerCallBack
     public AuctionStatus addAuction(Auction auction) {
 
-        if (auction != null) {
-            if (auctionDao.addAuction(auction)) {
-                startAuctionTimer(auction);
-                return AuctionStatus.AUCTION_ADDED;
-            }
+        if (auctionDao.addAuction(auction).equals(auction)) {
+            startAuctionTimer(auction);
+            return AuctionStatus.AUCTION_ADDED;
         }
+
         return AuctionStatus.AUCTION_NOT_ADDED;
     }
 
-    public void startAuctionTimer(Auction auction) {
+    private void startAuctionTimer(Auction auction) {
         Listener listener = new AuctionListener(auction.getAuctionId());
-        timers.put(auction.getAuctionId(), new TimerService(auction.getEndTimeInSeconds(), listener));
+        timers.put(auction.getAuctionId(), new TimerService(auction.getEndTime(), listener));
     }
-
 
     public List<Auction> getOpenAuctions() {
         return auctionDao.getRunningAuctions();
     }
 
-    public Auction getAuction(UUID auctionId) {
+    public Optional<Auction> getAuction(UUID auctionId) {
         return auctionDao.getAuction(auctionId);
     }
 
+    public BidStatus placeBid(UUID auctionId, Bid bid) {
 
-    //function should have one level of abstraction
-    //refactor the placeBid method and create the 4 method
+        Optional<Auction> auction = auctionDao.getAuction(auctionId);
 
-    public BidStatus placeBid(UUID auctionId, Bid bid)  {
-        Auction auction = auctionDao.getAuction(auctionId);
+        if (auction.isPresent() && auction.get().isAuctionOpen()) {
 
-        checkBalance(bid);
+            checkBalance(bid);
+            isPresentBidGreaterThenCurrentBid(auction.get(), bid);
 
-        if (auction.getIsAuctionOpen())
-            return updateCurrentBid(auction, bid);
+            if (updateCurrentBid(auction.get(), bid).equals(auction.get()))
+                return BidStatus.BID_ADDED;
+            else
+                return BidStatus.BID_NOT_ADDED;
+        }
         else
             throw new InvalidAuction("Auction is closed now ");
     }
@@ -82,24 +78,22 @@ public class AuctionService {
 
         if (paymentGateWay.checkSufficientBalance(userId, bidPrice).equals(BalanceStatus.NOT_SUFFICIENT_BALANCE))
             throw new InSufficientBalance("Not SufficientBalance");
+
     }
 
-    private BidStatus updateCurrentBid(Auction auction, Bid bid) {
+    private void isPresentBidGreaterThenCurrentBid(Auction auction, Bid bid) {
         BigDecimal currentBidPrice = auction.getCurrentBid().getBidPrice();
         BigDecimal presentBidPrice = bid.getBidPrice();
 
-        if ((currentBidPrice.compareTo(presentBidPrice) < 0))
-            return addBidToAuction(auction.getAuctionId(), bid);
-        else
+        if ((currentBidPrice.compareTo(presentBidPrice) >= 0)) {
             throw new InvalidBid("Bid price is lower than the current bid");
+        }
+
     }
 
-    private BidStatus addBidToAuction(UUID auctionId, Bid bid) {
-
-        if (bidDao.updateCurrentBid(auctionId, bid))
-            return BidStatus.BID_ADDED;
-        else
-            return BidStatus.BID_NOT_ADDED;
+    private Auction updateCurrentBid(Auction auction, Bid bid) {
+        auction.setCurrentBid(bid);
+        return auctionDao.update(auction);
     }
 
 }
